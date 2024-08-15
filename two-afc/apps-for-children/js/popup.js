7153d9c11aa691143590ecee6025afe012756858
ComonRaven <moonandstar118@gmail.com>
2024-08-15 17:18:33 +0900

    [佑作] 8/15 ログアウトボタンのポップアップ表示

--- two-afc/apps-for-children/js/popup.js
+++ two-afc/apps-for-children/js/popup.js
@@ -3,21 +3,21 @@ const closePopupButton = document.getElementById("closePopup");
 const overlay = document.getElementById("overlay");
 const popup = document.getElementById("popup");
 
-openPopupButton.addEventListener("click", 
-  () => {
-    overlay.style.display = "block";
-    popup.style.display = "block";
-  }
-);
-closePopupButton.addEventListener("click", 
-  () => {
-    overlay.style.display = "none";
-    popup.style.display = "none";
-  }
-);
-overlay.addEventListener("click", 
-  () => {
-    overlay.style.display = "none";
-    popup.style.display = "none";
-  }
-)
+if (openPopupButton && closePopupButton && overlay && popup) {
+    openPopupButton.addEventListener("click", () => {
+        overlay.style.display = "block";
+        popup.style.display = "block";
+    });
+
+    closePopupButton.addEventListener("click", () => {
+        overlay.style.display = "none";
+        popup.style.display = "none";
+    });
+
+    overlay.addEventListener("click", () => {
+        overlay.style.display = "none";
+        popup.style.display = "none";
+    });
+} else {
+    console.error("One or more elements not found");
+}
