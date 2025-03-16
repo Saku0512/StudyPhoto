c618b5bf1a78a73c54c7fbe9faa37bca07ff5286
Saku0512 <moonandstar118@gmail.com>
2025-03-16 22:12:34 +0900

    入力画面UI完成

--- two-afc/apps-for-children/contact.php
+++ two-afc/apps-for-children/contact.php
@@ -1,16 +1,30 @@
 <?php
 session_start();
+$nonce = base64_encode(random_bytes(16));
+header("Content-Security-Policy: default-src 'self'; script-src 'self' 'nonce-" . $nonce . "'; style-src 'self' 'nonce-" . $nonce . "';");
+
 $mode = 'input';
 $errmessage = array();
-$mymail = 'comonraven113@gmail.com';
 
 // CSRFトークンの生成
 if (empty($_SESSION['csrf_token'])) {
     $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
 }
 
+use Dotenv\Dotenv;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+
+require './vendor/phpmailer/phpmailer/src/Exception.php';
+require './vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require './vendor/phpmailer/phpmailer/src/SMTP.php';
+require './vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable('./');
+$dotenv->load();
+
+$Username = $_ENV['MAIL_USERNAME'] ?? $_SERVER['MAIL_USERNAME'];
+$Password = $_ENV['MIAL_APP_PASSWORD'] ?? $_SERVER['MIAL_APP_PASSWORD'];
 
 // CSRFトークンの確認
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -74,8 +88,8 @@ if (isset($_POST['back']) && $_POST['back']) {
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com'; // 自分のSMTPサーバーを設定
         $mail->SMTPAuth   = true;
-        $mail->Username   = ''; // SMTPユーザー名
-        $mail->Password   = ''; // SMTPパスワード
+        $mail->Username   = $Username; // SMTPユーザー名
+        $mail->Password   = $Password; // SMTPパスワード
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port       = 587; // ポート番号
 
@@ -105,9 +119,9 @@ if (isset($_POST['back']) && $_POST['back']) {
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width , initial-scale=1.0" />
-    <link rel="stylesheet" href="../css/main.css" />
-    <link rel="stylesheet" href="../css/scss/load.css" />
-    <script src="../js/load.js" defer></script>
+    <link rel="stylesheet" href="./css/contact.css" />
+    <link rel="stylesheet" href="./css/scss/load.css" />
+    <script src="./js/load.js" nonce="<?php htmlspecialchars($nonce, ENT_QUOTES, 'UTF-8') ?>" defer></script>
     <title>お問い合わせ</title>
 </head>
 <body>
@@ -123,27 +137,16 @@ if (isset($_POST['back']) && $_POST['back']) {
             <?php endif; ?>
             <form method="post" action="./contact.php">
                 <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>">
+                <p class="title">問い合わせフォーム</p>
                 <div class="Form">
-                    <div class="Form-Item">
-                        <div class="Form-Item-Label">
-                            <span class="Form-Item-Label-Required">必須</span>名前
-                        </div>
-                        <input type="text" name="fullname" id="name" class="Form-Item-Input" value="<?php echo $_SESSION['fullname'] ?? ''; ?>" required>
+                    <input class="FormName" type="text" name="name" placeholder="Name" required />
+                    <input class="FormEmail" type="email" name="email" placeholder="Eamil" required />
+                    <input class="FormSubject" type="text" name="subject" placeholder="Subject" required />
+                    <textarea class="FormContent" type="text" name="content" placeholder="Message" required></textarea>
+                    <div class="FormButtons">
+                        <button class="cancel" type="button">Back</button>
+                        <button class="submit" type="submit">Confirm</button>
                     </div>
-                    <div class="Form-Item">
-                        <div class="Form-Item-Label">
-                            <span class="Form-Item-Label-Required">必須</span>メールアドレス
-                        </div>
-                        <input type="email" name="email" id="mail" class="Form-Item-Input" value="<?php echo $_SESSION['email'] ?? ''; ?>" required>
-                    </div>
-                    <div class="Form-Item">
-                        <div class="Form-Item-Label">
-                            <span class="Form-Item-Label-Required">必須</span>お問い合わせ内容
-                        </div>
-                        <textarea class="Form-Item-Textarea" name="message" id="text" required><?php echo $_SESSION['message'] ?? ''; ?></textarea>
-                    </div>
-                    <input type="reset" class="cancel" value="リセット">
-                    <input type="submit" name="confirm" class="save" value="確認する">
                 </div>
             </form>
         <?php } else if ($mode == 'confirm') { ?>
